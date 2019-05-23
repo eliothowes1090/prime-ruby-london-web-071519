99e72f97de0a381a7d7bcc5e8af7771aad97bd1a
Eliot Howes <ejhowes@gmail.com>
2019-05-23 22:45:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(number)
   (2..(number - 1)).each do |n|
     if number % n == 0
       return false
-    elsif number < 2
-      return false 
+    elsif number < 0
+      return false
     end
   end
   true
