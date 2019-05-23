5902451080ff8b7a9c73059087420a444e10eed8
Eliot Howes <ejhowes@gmail.com>
2019-05-23 22:50:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,29 @@
 # Add  code here!
 def prime?(number)
+  if number < 2
+    return false
+
   (2..(number - 1)).each do |n|
-    if number < 2
-      return false
-    elsif number % n == 0
+    if number % n == 0
         return false
     end
   end
   true
+end
+
+
+def prime?(num)
+  # check if num is less than 2.
+  false if num < 2
+
+  # check if num is divisible by any number
+  # between 2 to (num - 1)
+  # return false on the first instance
+  (2...num).each do |factor|
+    if (num % factor).zero?
+      return false
+    end
+  end
+
+  true # after checking all numbers and not receiving false
 end
